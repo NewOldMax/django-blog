57352374d01bdf51929422d6dfc5fba4072a2f63
m.volkov <Maksim.Volkov@ocsico.com>
2016-07-04 10:20:25 +0300

    Enable flash messages.

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/blog/static/'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
